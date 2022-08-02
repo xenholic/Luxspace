0909909f7d711057a79458b05305dcbb97d8b88d
xenholic <zenobiaholic@gmail.com>
2022-08-02 22:59:42 +0700

    Finish layout on cart page

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,16 @@
 import { Route, Routes } from "react-router-dom"
 import HomePage from './pages/HomePage';
+import Details from './pages/Details';
+import Cart from './pages/Cart';
 
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route excact path="/" element={<HomePage />} />
+        <Route path="/categories/:idc" element={<Details />} />
+        <Route path="/cart" element={<Cart />} />
       </Routes>
     </div>
   );
--- client/src/pages/Details.js
+++ client/src/pages/Details.js
@@ -1,9 +1,11 @@
 import React from "react";
 import Header from "../parts/HomePage/Header";
-import Breadcrumb from "../parts/HomePage/Breadcrumb";
+import Breadcrumb from "../components/Breadcrumb";
 import Clients from "../parts/HomePage/Clients";
 import Sitemap from "../parts/HomePage/Sitemap";
 import Footer from "../parts/HomePage/Footer";
+import ProductDetails from "../parts/Details/ProductDetails";
+import Suggestion from "../parts/Details/Suggestion";
 
 export default function HomePage() {
     return (
@@ -14,6 +16,8 @@ export default function HomePage() {
                 { url: "/categories/91231", name: "office Room" },
                 { url: "/categories/91231/products/7111", name: "Details" },
             ]} />
+            <ProductDetails />
+            <Suggestion />
             <Clients />
             <Sitemap />
             <Footer />
